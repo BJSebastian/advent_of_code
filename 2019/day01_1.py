afb61a08f8d5676b065687306b553ae86a813995
Bryan Sebastian <BJSebastian@users.noreply.github.com>
2020-01-17 08:59:16 -0600

    initial commit w 2019 day 1 challenges

---
+++ 2019/day01_1.py
@@ -0,0 +1,53 @@
+#===========================================================================================
+# --- Day 1: The Tyranny of the Rocket Equation ---
+# --- Part One ---
+#===========================================================================================
+import math
+
+def fuel_counter_upper(mass):
+
+  # Specifically, to find the fuel required for a module, take its mass, divide by three,
+  fuel_required = mass / 3;
+
+  # round down,
+  fuel_required = math.floor(fuel_required)
+
+  # and subtract 2.
+  fuel_required -= 2
+
+  return fuel_required
+  
+
+def main():
+
+  # test run from examples provided in spec.
+  # modules_mass = [12, 14, 1969, 100756, 142388]
+  # for mass in modules_mass:
+  #   print(fuel_counter_upper(mass))
+
+  fuel_requirements = []
+  total_fuel_requirement = 0
+
+  try:
+    file_name = "day01.txt"
+
+    # open the file.
+    file_to_process = open(file_name,"r")
+
+    # process all lines in the file
+    for mass in file_to_process:
+      fuel_requirements.append(fuel_counter_upper(int(mass)))
+
+    for fuel_requirement in fuel_requirements:
+      # print(fuel_requirement)
+      total_fuel_requirement += fuel_requirement
+
+    print(total_fuel_requirement)
+
+  except FileNotFoundError:
+    print("\tError!", file_name, " File not found\n")
+  
+  except ValueError:
+    print("\tError!", file_name, "contains non-numeric data\n")
+
+main()
---
+++ 2019/day01_2.py
@@ -0,0 +1,45 @@
+#===========================================================================================
+# --- Day 1: The Tyranny of the Rocket Equation ---
+# --- Part Two ---
+#===========================================================================================
+import math
+
+def fuel_counter_upper(mass):
+
+  # Specifically, to find the fuel required for a module, take its mass, divide by three,
+  fuel_required = mass / 3;
+
+  # round down,
+  fuel_required = math.floor(fuel_required)
+
+  # and subtract 2.
+  fuel_required -= 2
+
+  # use recursion to get the fuel needed to carry the fuel
+  if (fuel_required) > 0:
+    fuel_required += fuel_counter_upper(fuel_required)
+  else:
+    fuel_required = 0
+
+  return fuel_required
+  
+def main():
+
+  # test run from examples provided in spec.
+  # modules_mass = [14, 1969, 100756]
+  # for mass in modules_mass:
+  #   print(fuel_counter_upper(mass))
+
+  # run with test data provided.
+  total_fuel_requirement = 0
+
+  # open the file.
+  file_to_process = open("day01.txt","r")
+
+  # process all lines in the file
+  for mass in file_to_process:
+    total_fuel_requirement += fuel_counter_upper(int(mass))
+
+  print(total_fuel_requirement)
+
+main()
