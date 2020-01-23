ac1137acba483b4a5b198f0500e15041603ed50b
Bryan Sebastian <BJSebastian@users.noreply.github.com>
2020-01-23 09:50:29 -0600

    start of day07 puzzle in python

--- 2019/day06.py
+++ 2019/day06.py
@@ -10,12 +10,20 @@ def count_orbits(curr_node_value, orbits):
     count += count_orbits(orbits.get(curr_node_value), orbits)
   return count
 
+def get_orbit(current_obj, orbits):
+    if current_obj not in orbits: return []
+    return [orbits[current_obj]] + get_orbit(orbits[current_obj], orbits)
+
+# def get_path_between(start, end, orbits):
+#   start_path, end_path = count_orbits(start, orbits), count_orbits(end, orbits)
+
 def main():
   orbits = {}
   objects = set()
   
   try:
     file_name = "input06.txt"
+    # file_name = "input06_2_test.txt"
 
     # open the file.
     file_to_process = open(file_name,"r")
@@ -32,9 +40,32 @@ def main():
       # recursively count orbits (parents) for this node.
       node_orbits = count_orbits(obj, orbits)
       total_orbits += node_orbits
-      # print(f"Node {obj} has {node_orbits} orbits.")
+      print(f"Node {obj} has {node_orbits} orbits.")
 
     print(f"total orbits: {total_orbits}")
+    
+    # --- PART TWO ---
+    # get list of nodes from you to com.
+    start_path = get_orbit("YOU", orbits)
+    # get list of notes from santa to com.
+    end_path = get_orbit("SAN", orbits)
+    print(f"start path = {start_path}")
+    print(f"end path = {end_path}")
+
+    # find the (only) node that is in both lists of nodes (from santa to com and from you to com)
+    overlapping_point = [i for i in start_path if i in end_path][0]
+    print(f"overlapping point = {overlapping_point}")
+
+    # the common node index in your list of nodes will be the number of jumps from you to that common node
+    start_path_index = start_path.index(overlapping_point)
+    # the common node index in santas list of nodes will be the number jumps from santa to that common node
+    end_path_index = end_path.index(overlapping_point)
+    print(f"start path index = {start_path_index}")
+    print(f"end path index = {end_path_index}")
+    # the number of jumps from you to santa will be the sum of 
+    # the number of jumps from you to that common node +
+    # the number jumps from santa to that common node
+    print(f"part 2 answer: {start_path_index + end_path_index}")
 
   except FileNotFoundError:
     print("\tError!", file_name, " File not found\n")
---
+++ 2019/day07.py
@@ -0,0 +1,321 @@
+#==========================================================================================
+#  Advent of Code 2019
+#  --- Day 7: Amplification Circuit ---
+#==========================================================================================
+from itertools import permutations 
+
+def process_intcodes(intcodes, input1, input2):
+  
+  input1_used = False
+  output = 0
+  instruction_pointer = 0
+  process_intcodes = True
+  
+  print(f"Input is: {input}")
+  print("-------------------------------------------------------------------------------")
+
+  while process_intcodes:
+
+    input_entered = 0
+    parm1 = 0 
+    parm1_mode = 0
+    parm2 = 0
+    parm2_mode = 0
+    parm3 = 0
+
+    opcode = intcodes[instruction_pointer]
+
+    # print(f"opcode: {opcode}")
+
+    #---------------------------------------------------------------------------------------
+    #  Determine parameter mode for each parameter:
+    #---------------------------------------------------------------------------------------
+    if (len(str(opcode)) > 2):
+      parm1_mode = str(opcode)[-3:]
+      parm1_mode = int(parm1_mode[:1])
+    if (len(str(opcode)) > 3):
+      parm2_mode = str(opcode)[-4:]
+      parm2_mode = int(parm2_mode[:1])
+
+    #---------------------------------------------------------------------------------------
+    #  Determine opcode
+    #---------------------------------------------------------------------------------------
+    if (opcode == "99999" or opcode == "9999" or opcode == "999" or opcode == "99"):
+      opcode = "99"
+    elif (len(str(opcode)) > 1):
+      opcode = str(opcode)[-1:]
+
+    #---------------------------------------------------------------------------------------
+    #  Get parameter values.
+    #---------------------------------------------------------------------------------------
+    if (int(opcode) == 1 or int(opcode) == 2
+      or int(opcode) == 7 or int(opcode) == 8):
+
+      parm1 = int(intcodes[instruction_pointer+1])
+      parm2 = int(intcodes[instruction_pointer+2])
+      parm3 = int(intcodes[instruction_pointer+3])
+      print(f"process instruction at pos {instruction_pointer}: {intcodes[instruction_pointer]} {parm1} {parm2} {parm3}")
+
+    elif (int(opcode) == 5 or int(opcode) == 6):
+
+      parm1 = int(intcodes[instruction_pointer+1])
+      parm2 = int(intcodes[instruction_pointer+2])
+      print(f"process instruction at pos {instruction_pointer}: {intcodes[instruction_pointer]} {parm1} {parm2}")
+
+    elif (int(opcode) == 3 or int(opcode) == 4):
+
+      parm1 = int(intcodes[instruction_pointer+1])
+      print(f"process instruction at pos {instruction_pointer}: {intcodes[instruction_pointer]} {parm1}")
+
+    #---------------------------------------------------------------------------------------
+    #  Process the instruction.
+    #---------------------------------------------------------------------------------------
+    report_line = "                   : "
+
+    if int(opcode) == 1 or int(opcode) == 2:
+      #-------------------------------------------------------------------------------------
+      #  1 - Add 
+      #  2 - Multiply
+      #-------------------------------------------------------------------------------------
+      value1 = 0
+      value2 = 0
+
+      if parm1_mode == 1:
+        value1 = parm1
+        report_line += f"{parm1} " 
+        report_line += "* " if int(opcode) == 2 else "+ "
+      else:
+        value1 = intcodes[parm1]
+        report_line += f"value at pos {parm1}: {intcodes[parm1]} " 
+        report_line += "* " if int(opcode) == 2 else "+ "
+
+      if parm2_mode == 1:
+        value2 = parm2
+        report_line += f"{parm2}"
+      else:
+        value2 = intcodes[parm2]
+        report_line += f"value at pos {parm2}: {intcodes[parm2]}"
+
+      print(report_line)
+      print("-------------------------------------------------------------------------------")
+
+      if int(opcode) == 1:
+        intcodes[parm3] = int(value1) + int(value2)
+      elif int(opcode) == 2:
+        intcodes[parm3] = int(value1) * int(value2)
+      instruction_pointer += 4
+
+    elif int(opcode) == 3:
+      #-------------------------------------------------------------------------------------
+      #  3 - Input to position
+      #-------------------------------------------------------------------------------------
+      # input_entered = input("Hit us up with that input? ")
+      
+      if input1_used:
+        intcodes[parm1] = input2
+      else:
+        intcodes[parm1] = input1
+        input1_used = True
+
+      print(f"                   : place input: {input_entered} into pos: {parm1}")
+      print("-------------------------------------------------------------------------------")
+      instruction_pointer += 2
+
+    elif int(opcode) == 4:
+      #-------------------------------------------------------------------------------------
+      #  4 - Output from position
+      #-------------------------------------------------------------------------------------
+      if parm1_mode == 1:
+        print(f"                   : output is value {parm1}")
+      else:
+        print(f"                   : output of position {parm1}: {intcodes[parm1]}")
+      
+      output = intcodes[parm1]
+
+      print("-------------------------------------------------------------------------------")
+      instruction_pointer += 2
+
+    elif int(opcode) == 5:
+      #-------------------------------------------------------------------------------------
+      #  5 - Jump-if-True
+      #-------------------------------------------------------------------------------------
+        
+      # set 1st parameter value (based on mode).
+      if parm1_mode == 1:
+        value1 = parm1
+        report_line += f"Is {parm1} non-zero? " 
+      else:
+        value1 = intcodes[parm1]
+        report_line += f"Is value at pos {parm1}: {intcodes[parm1]} is non-zero? "
+
+      if int(value1) != 0:
+        # set 2nd parameter value (based on mode).
+        if parm2_mode == 1:
+          value2 = parm2
+          report_line += f"Set instruction pointer to value {parm2}" 
+        else:
+          value2 = intcodes[parm2]
+          report_line += f" -- set instruction pointer to value at pos {parm2}: {intcodes[parm2]}"
+        instruction_pointer = int(value2)
+      else:
+        instruction_pointer += 3
+      
+      print(report_line)
+      print("-------------------------------------------------------------------------------")
+
+    elif int(opcode) == 6:
+      #-------------------------------------------------------------------------------------
+      #  6 - Jump-if-False
+      #-------------------------------------------------------------------------------------
+
+      # set 1st parameter value (based on mode).
+      if parm1_mode == 1:
+        value1 = parm1
+        report_line += f"Is {parm1} zero? " 
+      else:
+        value1 = intcodes[parm1]
+        report_line += f"Is value at pos {parm1}: {intcodes[parm1]} is zero? "
+
+      if int(value1) == 0:
+        # set 2nd parameter value (based on mode).
+        if parm2_mode == 1:
+          value2 = parm2
+          report_line += f"Set instruction pointer to value {parm2}" 
+        else:
+          value2 = intcodes[parm2]
+          report_line += f" -- set instruction pointer to value at pos {parm2}: {intcodes[parm2]}"
+        instruction_pointer = int(value2)
+      else:
+        instruction_pointer += 3
+
+      print(report_line)
+      print("-------------------------------------------------------------------------------")
+
+    elif int(opcode) == 7:
+      #-------------------------------------------------------------------------------------
+      #  7 - Less Than
+      #-------------------------------------------------------------------------------------
+
+      # set 1st parameter value (based on mode).
+      if parm1_mode == 1:
+        value1 = parm1
+        report_line += f"Is {parm1} < " 
+      else:
+        value1 = intcodes[parm1]
+        report_line += f"Id value at pos {parm1}: {intcodes[parm1]} < " 
+
+      # set 2nd parameter value (based on mode).
+      if parm2_mode == 1:
+        value2 = parm2
+        report_line += f"{parm2} ?" 
+      else:
+        value2 = intcodes[parm2]
+        report_line += f"value at pos {parm2}: {intcodes[parm2]} ?" 
+
+      if int(value1) < int(value2):
+        intcodes[parm3] = 1
+      else:
+        intcodes[parm3] = 0
+
+      print(report_line)
+      print("-------------------------------------------------------------------------------")
+      instruction_pointer += 4
+
+    elif int(opcode) == 8:
+      #-------------------------------------------------------------------------------------
+      #  8 - Equals
+      #-------------------------------------------------------------------------------------
+      # set 1st parameter value (based on mode).
+      if parm1_mode == 1:
+        value1 = parm1
+        report_line += f"Is {parm1} = " 
+      else:
+        value1 = intcodes[parm1]
+        report_line += f"Id value at pos {parm1}: {intcodes[parm1]} = " 
+
+      # set 2nd parameter value (based on mode).
+      if parm2_mode == 1:
+        value2 = parm2
+        report_line += f"{parm2} ?" 
+      else:
+        value2 = intcodes[parm2]
+        report_line += f"value at pos {parm2}: {intcodes[parm2]} ?" 
+
+      if int(value1) == int(value2):
+        intcodes[parm3] = 1
+      else:
+        intcodes[parm3] = 0
+
+      print(report_line)
+      print("-------------------------------------------------------------------------------")
+      instruction_pointer += 4
+
+    elif int(opcode) == 99:
+      #-------------------------------------------------------------------------------------
+      #  99 - Halt
+      #-------------------------------------------------------------------------------------
+      print(f"Processing opcode: {opcode} - Halt and Catch Fire!")
+      process_intcodes = False
+      break
+
+    else:
+      print("Unknown opcode encountered")
+      process_intcodes = False
+      break
+
+  return output
+
+def main():
+
+  # intcode program from test data...
+  # data = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
+  # seq_tuple = (4, 3, 2, 1, 0)
+
+  # test 2...
+  # data = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
+  # seq_tuple = (5, 4, 3, 2, 1)
+
+  # test 3...
+  data = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
+  seq_tuple = (1, 0, 4, 3, 2)
+
+  # Amplifier A
+  intcodes = data.split(",")
+  amp_output = 0
+  amp_output = process_intcodes(intcodes, seq_tuple[0], amp_output)
+  print(f"Amp Output: {amp_output}")
+
+  # Amplifier B
+  intcodes = data.split(",")
+  amp_output = process_intcodes(intcodes, seq_tuple[1], amp_output)
+  print(f"Amp Output: {amp_output}")
+
+  # Amplifier C
+  intcodes = data.split(",")
+  amp_output = process_intcodes(intcodes, seq_tuple[2], amp_output)
+  print(f"Amp Output: {amp_output}")
+
+  # Amplifier D
+  intcodes = data.split(",")
+  amp_output = process_intcodes(intcodes, seq_tuple[3], amp_output)
+  print(f"Amp Output: {amp_output}")
+
+  # Amplifier E
+  intcodes = data.split(",")
+  amp_output = process_intcodes(intcodes, seq_tuple[4], amp_output)
+  print(f"Amp Output: {amp_output}")
+
+  # Permutations...
+  # # A Python program to print all  
+  # # permutations using library function 
+
+  # # Get all permutations of [1, 2, 3] 
+  # perm = permutations([0, 1, 2, 3, 4]) 
+
+  # # Print the obtained permutations
+  # count = 1
+  #   for i in list(perm): 
+  #   print(f"{count}. {i}")
+  #   count += 1
+        
+main()
