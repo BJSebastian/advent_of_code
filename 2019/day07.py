c11f5dde9d0b88c976ba7a4181c6f6ba9aaf7c24
Bryan Sebastian <BJSebastian@users.noreply.github.com>
2020-01-23 15:27:04 -0600

    day 7 add python permutations
    Add permutation to get all possible sequences that need to run through the int code processor.

--- 2019/day07.py
+++ 2019/day07.py
@@ -11,8 +11,8 @@ def process_intcodes(intcodes, input1, input2):
   instruction_pointer = 0
   process_intcodes = True
   
-  print(f"Input is: {input}")
-  print("-------------------------------------------------------------------------------")
+  # print(f"Input is: {input1}, {input2}")
+  # print("-------------------------------------------------------------------------------")
 
   while process_intcodes:
 
@@ -54,18 +54,18 @@ def process_intcodes(intcodes, input1, input2):
       parm1 = int(intcodes[instruction_pointer+1])
       parm2 = int(intcodes[instruction_pointer+2])
       parm3 = int(intcodes[instruction_pointer+3])
-      print(f"process instruction at pos {instruction_pointer}: {intcodes[instruction_pointer]} {parm1} {parm2} {parm3}")
+      # print(f"process instruction at pos {instruction_pointer}: {intcodes[instruction_pointer]} {parm1} {parm2} {parm3}")
 
     elif (int(opcode) == 5 or int(opcode) == 6):
 
       parm1 = int(intcodes[instruction_pointer+1])
       parm2 = int(intcodes[instruction_pointer+2])
-      print(f"process instruction at pos {instruction_pointer}: {intcodes[instruction_pointer]} {parm1} {parm2}")
+      # print(f"process instruction at pos {instruction_pointer}: {intcodes[instruction_pointer]} {parm1} {parm2}")
 
     elif (int(opcode) == 3 or int(opcode) == 4):
 
       parm1 = int(intcodes[instruction_pointer+1])
-      print(f"process instruction at pos {instruction_pointer}: {intcodes[instruction_pointer]} {parm1}")
+      # print(f"process instruction at pos {instruction_pointer}: {intcodes[instruction_pointer]} {parm1}")
 
     #---------------------------------------------------------------------------------------
     #  Process the instruction.
@@ -96,8 +96,8 @@ def process_intcodes(intcodes, input1, input2):
         value2 = intcodes[parm2]
         report_line += f"value at pos {parm2}: {intcodes[parm2]}"
 
-      print(report_line)
-      print("-------------------------------------------------------------------------------")
+      # print(report_line)
+      # print("-------------------------------------------------------------------------------")
 
       if int(opcode) == 1:
         intcodes[parm3] = int(value1) + int(value2)
@@ -117,22 +117,22 @@ def process_intcodes(intcodes, input1, input2):
         intcodes[parm1] = input1
         input1_used = True
 
-      print(f"                   : place input: {input_entered} into pos: {parm1}")
-      print("-------------------------------------------------------------------------------")
+      # print(f"                   : place input: {input_entered} into pos: {parm1}")
+      # print("-------------------------------------------------------------------------------")
       instruction_pointer += 2
 
     elif int(opcode) == 4:
       #-------------------------------------------------------------------------------------
       #  4 - Output from position
       #-------------------------------------------------------------------------------------
-      if parm1_mode == 1:
-        print(f"                   : output is value {parm1}")
-      else:
-        print(f"                   : output of position {parm1}: {intcodes[parm1]}")
+      # if parm1_mode == 1:
+      #   print(f"                   : output is value {parm1}")
+      # else:
+      #   print(f"                   : output of position {parm1}: {intcodes[parm1]}")
       
       output = intcodes[parm1]
 
-      print("-------------------------------------------------------------------------------")
+      # print("-------------------------------------------------------------------------------")
       instruction_pointer += 2
 
     elif int(opcode) == 5:
@@ -160,8 +160,8 @@ def process_intcodes(intcodes, input1, input2):
       else:
         instruction_pointer += 3
       
-      print(report_line)
-      print("-------------------------------------------------------------------------------")
+      # print(report_line)
+      # print("-------------------------------------------------------------------------------")
 
     elif int(opcode) == 6:
       #-------------------------------------------------------------------------------------
@@ -188,8 +188,8 @@ def process_intcodes(intcodes, input1, input2):
       else:
         instruction_pointer += 3
 
-      print(report_line)
-      print("-------------------------------------------------------------------------------")
+      # print(report_line)
+      # print("-------------------------------------------------------------------------------")
 
     elif int(opcode) == 7:
       #-------------------------------------------------------------------------------------
@@ -217,8 +217,8 @@ def process_intcodes(intcodes, input1, input2):
       else:
         intcodes[parm3] = 0
 
-      print(report_line)
-      print("-------------------------------------------------------------------------------")
+      # print(report_line)
+      # print("-------------------------------------------------------------------------------")
       instruction_pointer += 4
 
     elif int(opcode) == 8:
@@ -246,15 +246,15 @@ def process_intcodes(intcodes, input1, input2):
       else:
         intcodes[parm3] = 0
 
-      print(report_line)
-      print("-------------------------------------------------------------------------------")
+      # print(report_line)
+      # print("-------------------------------------------------------------------------------")
       instruction_pointer += 4
 
     elif int(opcode) == 99:
       #-------------------------------------------------------------------------------------
       #  99 - Halt
       #-------------------------------------------------------------------------------------
-      print(f"Processing opcode: {opcode} - Halt and Catch Fire!")
+      # print(f"Processing opcode: {opcode} - Halt and Catch Fire!")
       process_intcodes = False
       break
 
@@ -273,49 +273,67 @@ def main():
 
   # test 2...
   # data = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
-  # seq_tuple = (5, 4, 3, 2, 1)
+  # seq_tuple = (0, 1, 2, 3, 4)
 
   # test 3...
-  data = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
-  seq_tuple = (1, 0, 4, 3, 2)
+  # data = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
+  # seq_tuple = (1, 0, 4, 3, 2)
 
-  # Amplifier A
-  intcodes = data.split(",")
-  amp_output = 0
-  amp_output = process_intcodes(intcodes, seq_tuple[0], amp_output)
-  print(f"Amp Output: {amp_output}")
+  # open the file.
+  with open('input07.txt', 'r') as file:
+    data = file.read()
 
-  # Amplifier B
-  intcodes = data.split(",")
-  amp_output = process_intcodes(intcodes, seq_tuple[1], amp_output)
-  print(f"Amp Output: {amp_output}")
+    # seq_tuple = (0, 1, 2, 3, 4)
+    # Get all permutations of [1, 2, 3] 
+    perm = permutations([0, 1, 2, 3, 4]) 
 
-  # Amplifier C
-  intcodes = data.split(",")
-  amp_output = process_intcodes(intcodes, seq_tuple[2], amp_output)
-  print(f"Amp Output: {amp_output}")
+    count = 1
+    highest_output_signal = 0
 
-  # Amplifier D
-  intcodes = data.split(",")
-  amp_output = process_intcodes(intcodes, seq_tuple[3], amp_output)
-  print(f"Amp Output: {amp_output}")
+    for seq_tuple in list(perm): 
+      amp_output_signal = 0
+      # print("-------------------------------------------------------------------------------")
+      for x in seq_tuple:
+        intcodes = data.split(",")
+        amp_output_signal = process_intcodes(intcodes, x, amp_output_signal)
+        # print(f"Amp Output: {amp_output_signal}")
+        # print("-------------------------------------------------------------------------------")
 
-  # Amplifier E
-  intcodes = data.split(",")
-  amp_output = process_intcodes(intcodes, seq_tuple[4], amp_output)
-  print(f"Amp Output: {amp_output}")
+      if amp_output_signal > highest_output_signal:
+        highest_output_signal = amp_output_signal
+      print(f"run #{count} output is: {amp_output_signal}")
+      count += 1
 
-  # Permutations...
-  # # A Python program to print all  
-  # # permutations using library function 
+  print(f"Highest signal that can be sent to the thrusters?: {highest_output_signal}")
 
-  # # Get all permutations of [1, 2, 3] 
-  # perm = permutations([0, 1, 2, 3, 4]) 
+  #----------------------------------------------------------------------------------------
+  #  Simple run through...
+  #----------------------------------------------------------------------------------------
+  
+  # # Amplifier A
+  # intcodes = data.split(",")
+  # amp_output = 0
+  # amp_output = process_intcodes(intcodes, seq_tuple[0], amp_output)
+  # print(f"Amp Output: {amp_output}")
 
-  # # Print the obtained permutations
-  # count = 1
-  #   for i in list(perm): 
-  #   print(f"{count}. {i}")
-  #   count += 1
+  # # Amplifier B
+  # intcodes = data.split(",")
+  # amp_output = process_intcodes(intcodes, seq_tuple[1], amp_output)
+  # print(f"Amp Output: {amp_output}")
+
+  # # Amplifier C
+  # intcodes = data.split(",")
+  # amp_output = process_intcodes(intcodes, seq_tuple[2], amp_output)
+  # print(f"Amp Output: {amp_output}")
+
+  # # Amplifier D
+  # intcodes = data.split(",")
+  # amp_output = process_intcodes(intcodes, seq_tuple[3], amp_output)
+  # print(f"Amp Output: {amp_output}")
+
+  # # Amplifier E
+  # intcodes = data.split(",")
+  # amp_output = process_intcodes(intcodes, seq_tuple[4], amp_output)
+  # print(f"Amp Output: {amp_output}")
         
 main()
