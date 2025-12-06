d517df7a7f0168eff84bcf3b75194f86e76a582f
Bryan Sebastian <BJSebastian@users.noreply.github.com>
2025-12-06 15:06:07 -0600

    Add day0 and day1 template

---
+++ 2025/day00/index.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+const fs = require("fs");
+
+async function readInput() {
+  const input = fs.readFileSync("sample.txt", "utf-8");
+  // const input = fs.readFileSync('input.txt', 'utf-8');
+  return input.split("\n").map((x) => x);
+}
+
+async function puzzle() {
+  // Get each row into an array.
+  const rows = await readInput();
+  let currPosition = 50;
+  let positionsAtZero = 0;
+
+  rows.forEach((row) => {
+    // console.log(`row: ${row}`);
+    if (row.length > 0) {
+      let positions = parseInt(row.slice(1), 10);
+      if (row[0] === "L") {
+				if (positions > 100) {
+					positions = parseInt(row.slice(-2), 10);
+				}
+        currPosition -= positions;
+        if (currPosition < 0) {
+          currPosition += 100;
+        }
+        console.log(`rotate left ${positions} positions to point at ${currPosition}`);
+      } else {
+				if (positions > 100) {
+					positions = parseInt(row.slice(-2), 10);
+				}
+        currPosition += positions;
+        if (currPosition >= 100) {
+          currPosition -= 100;
+        }
+        console.log(`rotate right: ${positions} positions to point at ${currPosition}`);
+      }
+      if (currPosition === 0) {
+        positionsAtZero += 1;
+      }
+    }
+  });
+  console.log(`Positions at zero: ${positionsAtZero}`);
+}
+
+puzzle();
